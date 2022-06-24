33d862bbd2d854844e305f75c0d592bf05bb5e8f
Kevin Eduardo Cardenas mamani <kevin.kc541@gmail.com>
2022-06-24 16:46:14 -0400

    Y finalizamos, no se me ocurre nada más xD

--- Expresión 5/Ejercicio 5b.cpp
+++ Expresión 5/Ejercicio 5b.cpp
@@ -7,7 +7,17 @@ using namespace std;
 
 int main()
 {
+	double x, y, aux;
 
+	cout << "Introduzca un numero: " << endl; cin >> x;
+	cout << "Introduzca otro numero: \n", cin >> y;
+
+	aux = x;
+	x = y;
+	y = aux;
+
+	cout << "El nuevo valor de x es: " << x << endl;
+	cout << "El nuevo valor de y es: " << y << endl;
 
 	return 0;
 }
